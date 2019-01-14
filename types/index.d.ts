76d4528790b73396b9f56b0dc049f5c8cd916b95
bySabi Files <bysabi.files@gmail.com>
2019-01-14 23:25:41 +0100

    update dependencies

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,2 @@
 // node vs react-native global conflict workaround: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/16825
 declare var process: any;
-
-declare module 'react-lifecycles-compat' {
-    import * as React from 'react';
-    export function polyfill<P>(
-      Comp: React.ComponentType<P>
-    ): React.ComponentType<P>;
-  }
