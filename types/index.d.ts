f9f0dfb6572179bc2606a46abefaab65164baf3a
bySabi Files <bysabi.files@gmail.com>
2019-01-17 08:22:55 +0100

    update dependencies

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,3 @@
 // Workaround for @types/node vs @types/react-native definitions conflict:
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/16825
 declare var process: any;
-
-// TODO: remove when react-lifecycles-compat be updated on DefinitelyTyped
-declare module 'react-lifecycles-compat' {
-  import * as React from 'react';
-  export function polyfill<T extends React.ComponentType<any>>(
-    Comp: T
-  ): T & { [K in keyof T]: T[K] };
-}
